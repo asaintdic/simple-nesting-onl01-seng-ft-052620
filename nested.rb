38346be8b31449217ca24d95a04b7f780bde7395
Alain Saint-Dic <alains214@gmail.com>
2020-06-08 20:53:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     alan_known = programmer_hash[:alan_kay]
+     return alan_known[:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     den_lang = programmer_hash[:dennis_ritchie]
+     return den_lang[:languages].join
 end
 
 def adding_matz
@@ -81,7 +85,14 @@ def adding_matz
      }
 
     
+   {:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }}.merge(programmer_hash)
+
 end
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -102,6 +113,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     alan_known = programmer_hash[:alan_kay]
+      alan_known[:known_for] = alans_new_info
+      return :alan_kay[:known_for]
      
 end
 
